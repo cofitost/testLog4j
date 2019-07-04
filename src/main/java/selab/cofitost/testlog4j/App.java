31decd3e88c97d240f827d9875337c3122bcc22a
cofitost <zxcv091922@gmail.com>
2019-07-05 04:02:37 +0800

    filter the "fatal" level

--- src/main/java/selab/cofitost/testlog4j/App.java
+++ src/main/java/selab/cofitost/testlog4j/App.java
@@ -1,13 +1,21 @@
 package selab.cofitost.testlog4j;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  private static final Logger logger = LogManager.getLogger(App.class);
+
+  public static void main(String[] args) {
+
+    logger.debug("debug");
+    logger.info("info");
+    logger.warn("warn");
+    logger.error("error");
+    logger.fatal("fatal");
+  }
 }
